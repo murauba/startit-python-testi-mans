f9c88606a76706ff05f991b72f10afbd9592d4f7
Gundega Dekena <gundega.dekena@gmail.com>
2020-11-11 02:56:54 +0200

    add string uzdevums

---
+++ src/013-sveiciens.py
@@ -0,0 +1,8 @@
+""" 
+Uzrakstiet programmu, kas ielasa vienu vārdu 
+un izvada uz ekrāna sveicienu sekojošā formātā:
+"Labrīt, vards!"
+Piemēram, ja ievadīts ir vārds "Māris", tad tiek izvadīts:
+"Labrīt, Māris!"
+Pārbaudiet programmas darbību ar dažādiem ievaddatiem.
+"""
